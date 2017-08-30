db6bb32d406199b38383d9de8c66aa44597c920b
Guanjie Wang <wanggj@shanghaitech.edu.cn>
2017-08-30 11:46:48 +0800

    add test_baryWeights.m to test baryWeights
    add test_baryWeights.m to test the consistency of barycentric weight.

---
+++ tests/misc/test_baryWeights.m
@@ -0,0 +1,191 @@
+function pass = test_baryWeights(pref)
+
+if ( nargin == 0 ) 
+    pref = chebfunpref();
+end
+
+% NOTE: We only test the consistency of barycentric weights equal 1 or -1.
+
+
+% consistency of first Chebyshev points
+xpts = 'chebpts'; j = 0; a = 1;
+
+[x, ~, v] = feval(xpts,5,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+1) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,6,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+2) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2003,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+3) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2004,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+4) = norm( v(id)-vb(id),inf ) < eps; 
+
+% consistency of second Chebyshev points
+xpts = 'chebpts';j = 4; a = 2;
+
+[x, ~, v] = feval(xpts,5,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+1) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,6,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+2) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2003,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+3) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2004,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+4) = norm( v(id)-vb(id),inf ) < eps; 
+
+
+% consistency of Legendre  points
+xpts = 'legpts'; j = 8;
+
+[x, ~, v] = feval(xpts,5);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+1) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,6);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+2) = norm( v(id)-vb(id),inf ) < eps; 
+
+
+[x, ~, v] = feval(xpts,2003);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+3) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2004);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+4) = norm( v(id)-vb(id),inf ) < eps; 
+
+
+% consistency of Legendre-Lobatto points
+xpts = 'lobpts'; j = 12;
+
+[x, ~, v] = feval(xpts,5);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+1) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,6);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+2) = norm( v(id)-vb(id),inf ) < eps; 
+
+
+[x, ~, v] = feval(xpts,2003);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+3) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2004);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+4) = norm( v(id)-vb(id),inf ) < eps; 
+
+
+% consistency of Legendre-Radau points
+xpts = 'radaupts'; j = 16;
+
+[x, ~, v] = feval(xpts,5);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+1) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,6);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+2) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2003);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+3) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2004);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+4) = norm( v(id)-vb(id),inf ) < eps; 
+
+
+% consistency of Hermite points
+
+xpts = 'hermpts'; j = 20;
+
+[x, ~, v] = feval(xpts,5);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+1) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,6);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+2) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2003);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+3) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2004);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+4) = norm( v(id)-vb(id),inf ) < eps; 
+
+
+% consistency of Ultraspherical points
+xpts = 'ultrapts'; j = 24; a = 1/4;
+
+[x, ~, v] = feval(xpts,5,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+1) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,6,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+2) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2003,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+3) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2004,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+4) = norm( v(id)-vb(id),inf ) < eps; 
+
+
+% consistency of Laguerre points 
+xpts = 'lagpts'; j = 28; a = 1/4;
+
+[x, ~, v] = feval(xpts,5,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+1) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,6,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+2) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2003,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+3) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2004,a);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+4) = norm( v(id)-vb(id),inf ) < eps;
+
+
+% consistency of Jacobi points 
+xpts = 'jacpts'; j =32; a = 1/4; b = 1/2;
+
+[x, ~, v] = feval(xpts,5,a,b);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+1) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,6,a,b);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+2) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2003,a,b);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+3) = norm( v(id)-vb(id),inf ) < eps; 
+
+[x, ~, v] = feval(xpts,2004,a,b);  
+vb = baryWeights(x); id = find(abs(vb) == 1);
+pass(j+4) = norm( v(id)-vb(id),inf ) < eps;
+
+end
